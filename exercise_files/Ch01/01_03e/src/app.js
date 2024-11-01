edf97b26b054aab06657a3d488dd6ecb12f8338c
Abdul Arif <abdul.arif0705@gmail.com>
2024-11-01 19:20:58 -0400

    comments

--- exercise_files/Ch01/01_03e/src/app.js
+++ exercise_files/Ch01/01_03e/src/app.js
@@ -1,7 +1,7 @@
 /**
- * 
- * @param {number} contactId 
- * @returns 
+ * @param makes it see problems because it's not type number if we go change 2 to "2" on line 24 you'll see ts complaining which is what we like to see
+ * @param {number} contactId
+ * @returns
  */
 async function getContact(contactId) {
   const resp = await $.ajax({
@@ -17,10 +17,10 @@ async function getContact(contactId) {
 }
 
 getContact(1).then((contact) => {
-  contact.id = 1234
+  contact.id = 1234;
   contact.birthDate = new Date("12/12/1990");
 });
 
-getContact(2).then((contact) => {
+getContact("2").then((contact) => {
   console.log("Contact: ", JSON.stringify(contact));
 });
