7505d29e492e3085c0cdccc2fdcb4ed2454b7d61
Abdul Arif <abdul.arif0705@gmail.com>
2024-11-07 16:51:19 -0500

    type alias is not that useful imo

--- exercise_files/Ch02/02_03b/src/demo.ts
+++ exercise_files/Ch02/02_03b/src/demo.ts
@@ -10,4 +10,4 @@ let primaryContact: Contact = {
   name: "Jamie Johnson",
 };
 
-type ContactName = string;
+type ContactName = string; // this is a type alias and it's just a string but the name is explicit so when we look on line 3 we know it's not just a string but a ContactName string... Honestly using string is more straightforward this seems retarded no cap
