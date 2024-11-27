52e26db0a4e470f68d226b1bfe3496cbb5ac00f8
Abdul Arif <abdul.arif0705@gmail.com>
2024-11-26 22:20:03 -0500

    continue TS

--- exercise_files/Ch02/02_04b/src/demo.ts
+++ exercise_files/Ch02/02_04b/src/demo.ts
@@ -5,7 +5,7 @@ interface Contact {
   status: ContatcStatus;
 }
 
-// Enums are a special type that has a hard-coded list of values and is defined like this. Once an enum is defined, I can refer to it just as I would any other type. In our contact case I can replace my string type with the ContactStatus enum.  Unlike many other TypeScripts syntaxes that get stripped away at compile time, enums do get compiled along with the rest of your code. This allows you to refer to these types at runtime like this
+// Enums (enumerable types) are a special type that has a hard-coded list of values and is defined like this. Once an enum is defined, I can refer to it just as I would any other type. In our contact case I can replace my string type with the ContactStatus enum.  Unlike many other TypeScripts syntaxes that get stripped away at compile time, enums do get compiled along with the rest of your code. This allows you to refer to these types at runtime like this
 enum ContatcStatus {
   Active = "Active",
   Inactive = "Inactive",
--- exercise_files/Ch03/03_01b/src/demo.ts
+++ exercise_files/Ch03/03_01b/src/demo.ts
@@ -1,39 +1,47 @@
 type ContactName = string;
 
-enum ContactStatus {
-    Active = "active",
-    Inactive = "inactive",
-    New = "new"
-}
+type ContactStatus = "active" | "inactive" | "new"; // turned enum into type alias instead for examples like this where you want to just restrict certain fields to a limited set of known values, I find type aliases to be far more effective. Type aliases are generally more performant because they are resolved at compile time
+// In summary, use type aliases when you need a simple and flexible way to define a set of possible values, and use enums when you need a more structured and feature-rich way to define named constants.
 
-interface Contact {
-    id: number;
-    name: ContactName;
-    birthDate?: Date;
-    status?: ContactStatus;
+// defining enums like this actually adds more code to your final output. Often that may be what you want.
+// enum ContactStatus {
+//   Active = "active",
+//   Inactive = "inactive",
+//   New = "new",
+// }
+
+type ContactBirthDate = Date | number | string; // define diff types it can take
+
+interface Contact extends Address {
+  id: number;
+  name: ContactName;
+  birthDate?: ContactBirthDate; // define diff types it can take we could also do
+  status?: ContactStatus; // the values will be restricted to the 3 values we added in the type alias above
 }
 
 interface Address {
-    line1: string;
-    line2: string;
-    province: string;
-    region: string;
-    postalCode: string;
+  line1: string;
+  line2: string;
+  province: string;
+  region: string;
+  postalCode: string;
 }
 
+// interface AddressableContact extends Contact, Address {}
+type AddressableContact = Contact & Address; // Combines the 2 types together to create a new type
+
 function getBirthDate(contact: Contact) {
-    if (typeof contact.birthDate === "number") {
-        return new Date(contact.birthDate);
-    }
-    else if (typeof contact.birthDate === "string") {
-        return Date.parse(contact.birthDate)
-    }
-    else {
-        return contact.birthDate
-    }
+  if (typeof contact.birthDate === "number") {
+    return new Date(contact.birthDate);
+  } else if (typeof contact.birthDate === "string") {
+    return Date.parse(contact.birthDate);
+  } else {
+    return contact.birthDate;
+  }
 }
 
 let primaryContact: Contact = {
-    id: 12345,
-    name: "Jamie Johnson"
-}
+  id: 12345,
+  name: "Jamie Johnson",
+  status: "active", // restricted to the values its type contains
+};
