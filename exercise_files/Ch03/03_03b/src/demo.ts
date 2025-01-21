b310e89e7fd19760fa6e32d97216ccec8c8c2552
Abdul Arif <abdul.arif0705@gmail.com>
2025-01-21 16:00:20 -0500

    typeof operator

--- exercise_files/Ch03/03_03b/src/demo.ts
+++ exercise_files/Ch03/03_03b/src/demo.ts
@@ -1,34 +1,37 @@
-const x = "string"
-const y = true
-console.log(typeof x) // --> "string"
-console.log(typeof y) // --> "boolean"
-
-
+const x = "string";
+const y = true;
+console.log(typeof x); // --> "string"
+console.log(typeof y); // --> "boolean"
 
 type ContactName = string;
-type ContactStatus = "active" | "inactive" | "new"
-type ContactBirthDate = Date | number | string
+type ContactStatus = "active" | "inactive" | "new";
+type ContactBirthDate = Date | number | string;
 
 interface Contact {
-    id: number;
-    name: ContactName;
-    birthDate?: ContactBirthDate;
-    status?: ContactStatus;
+  id: number;
+  name: ContactName;
+  birthDate?: ContactBirthDate;
+  status?: ContactStatus;
 }
 
-function toContact(nameOrContact) {
-    if (typeof nameOrContact === "object") {
-        return {
-            id: nameOrContact.id,
-            name: nameOrContact.name,
-            status: nameOrContact.status
-        }
-    }
-    else {
-        return {
-            id: 0,
-            name: nameOrContact,
-            status: "active"
-        }
-    }
+function toContact(nameOrContact: string | Contact): Contact {
+  if (typeof nameOrContact === "object") {
+    // because we said nameOrContact can be either a string or a Contact type, if it's a Contact type then it is an object hence TypeScript infers (hover over the nameOrContact.id or any one of them below and see it says ((parameter) nameOrContact: Contact) because it inferred that)
+    return {
+      id: nameOrContact.id,
+      name: nameOrContact.name,
+      status: nameOrContact.status,
+    };
+  } else {
+    return {
+      id: 0,
+      name: nameOrContact, // hover over this and it'll show you it inferred it's a string
+      status: "active",
+    };
+  }
 }
+
+// define or determine static types dynamically
+const myType = { min: 1, max: 200 };
+
+function save(source: typeof myType) {} // this is a shortcut but most cases better to just create an interface or type alias
