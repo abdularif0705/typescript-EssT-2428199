4a53947e70d2981760ad0887d42cf464e072a2e0
Abdul Arif <abdul.arif0705@gmail.com>
2025-01-20 21:40:56 -0500

    generic types

--- exercise_files/Ch03/03_02b/src/demo.ts
+++ exercise_files/Ch03/03_02b/src/demo.ts
@@ -1,16 +1,25 @@
 type ContactName = string;
-type ContactStatus = "active" | "inactive" | "new"
-type ContactBirthDate = Date | number | string
+type ContactStatus = "active" | "inactive" | "new";
+type ContactBirthDate = Date | number | string;
 
-interface Contact  {
-    id: number;
-    name: ContactName;
-    birthDate?: ContactBirthDate;
-    status?: ContactStatus;
+interface Contact {
+  id: number;
+  name: ContactName;
+  birthDate?: ContactBirthDate;
+  status?: ContactStatus;
+  email: string;
 }
 
 let primaryContact: Contact = {
-    id: 12345,
-    name: "Jamie Johnson",
-    status: "active"
+  id: 12345,
+  name: "Jamie Johnson",
+  status: "active",
+};
+
+type ContactFields = keyof Contact;
+
+function getValue<T, U extends keyof T>(source: T, propertyName: U) {
+  return source[propertyName]; // without strong guardrails this dynamic typing can cause hard to debug bugs for example
 }
+
+const value = getValue({ min: 1, max: 200 }, ""); // iif you press Ctrl + Space inside the "" it'll show max and min as options because TS has inferred the values
