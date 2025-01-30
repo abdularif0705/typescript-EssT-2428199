464f125f4042785f6eea95c50ec141d099ecdb35
Abdul Arif <abdul.arif0705@gmail.com>
2025-01-29 21:33:31 -0500

    More TS tricks -> keyof, Record, Omit, Partial, Required, Pick

--- exercise_files/Ch03/03_02b/src/demo.ts
+++ exercise_files/Ch03/03_02b/src/demo.ts
@@ -16,10 +16,17 @@ let primaryContact: Contact = {
   status: "active",
 };
 
-type ContactFields = keyof Contact;
+type ContactFields = keyof Contact; // variables of type ContactFields can only contain values matching Contact properties
+
+const field: ContactFields = "";
+
+// function getValue(source, propertyName) {
+//   return source[propertyName]; // without strong guardrails this dynamic typing can cause hard to debug bugs for example, look below for solution to this
+// }
 
 function getValue<T, U extends keyof T>(source: T, propertyName: U) {
-  return source[propertyName]; // without strong guardrails this dynamic typing can cause hard to debug bugs for example
+  // same thing as function getValue<T>(source: T, propertyName: keyof T) {
+  return source[propertyName];
 }
 
 const value = getValue({ min: 1, max: 200 }, ""); // iif you press Ctrl + Space inside the "" it'll show max and min as options because TS has inferred the values
--- exercise_files/Ch03/03_04b/src/demo.ts
+++ exercise_files/Ch03/03_04b/src/demo.ts
@@ -1,36 +1,48 @@
 type ContactStatus = "active" | "inactive" | "new";
 
 interface Address {
-    street: string;
-    province: string;
-    postalCode: string;
+  street: string;
+  province: string;
+  postalCode: string;
 }
 
 interface Contact {
-    id: number;
-    name: string;
-    status: ContactStatus;
-    address: Address;
+  id: number;
+  name: string;
+  status: ContactStatus;
+  address: Address;
 }
+
+type Awesome = Contact["address"]["postalCode"]; // reference address type in Contact interface and dig into address of type Address and reference IT'S property postalCode (hover cursor over the Awesome keyword and it'll say string)
 
 interface ContactEvent {
-    contactId: number;
+  contactId: Contact["id"];
 }
 
-interface ContactDeletedEvent extends ContactEvent { 
-}
+interface ContactDeletedEvent extends ContactEvent {}
 
-interface ContactStatusChangedEvent extends ContactEvent { 
-    oldStatus: ContactStatus;
-    newStatus: ContactStatus;
+interface ContactStatusChangedEvent extends ContactEvent {
+  oldStatus: Contact["status"];
+  newStatus: Contact["status"];
 }
 
 interface ContactEvents {
-    deleted: ContactDeletedEvent;
-    statusChanged: ContactStatusChangedEvent;
-    // ... and so on
+  deleted: ContactDeletedEvent;
+  statusChanged: ContactStatusChangedEvent; // detect this in last line
+  // ... and so on
 }
 
 function getValue<T, U extends keyof T>(source: T, propertyName: U) {
-    return source[propertyName];
+  return source[propertyName];
 }
+
+function handleEvent<T extends keyof ContactEvents>(
+  eventName: T, // accept a string containing any of th eproperties in ContactEvents
+  handler: (evt: ContactEvents[T]) => void // function that accepts a single parameter and that parameter
+) {
+  if (eventName === "statusChanged") {
+    handler({ contactId: 1, oldStatus: "active", newStatus: "inactive" }); // it takes ContactStatusChangedEvent object
+  }
+}
+
+handleEvent("statusChanged", (evt) => evt); // by doing index acess on line 41 it went statusChanged tyoe
