b3527b8887c9145793b4e383994b73cf23e5e80b
Abdul Arif <abdul.arif0705@gmail.com>
2025-02-06 20:39:36 -0500

    custom decorator parameters we can pass by using a decorator factory and inside of it returning the mandatory decorator with the descriptor parameter

--- exercise_files/Ch05/05_02b/src/demo.ts
+++ exercise_files/Ch05/05_02b/src/demo.ts
@@ -1,39 +1,61 @@
 interface Contact {
-    id: number;
+  id: number;
 }
 
 const currentUser = {
-    id: 1234,
-    roles: ["ContactEditor"],
-    isAuthenticated(): boolean {
-        return true
-    },
-    isInRole(role: string): boolean {
-        return this.roles.contains(role);
+  id: 1234,
+  roles: ["ContactEditor"],
+  isAuthenticated(): boolean {
+    return true;
+  },
+  isInRole(role: string): boolean {
+    return this.roles.contains(role);
+  },
+};
+
+function authorize(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  const wrapped = descriptor.value; // make a copy of the original method before we override it
+
+  descriptor.value = function () {
+    // change the method to this new one
+    if (!currentUser.isAuthenticated()) {
+      throw Error("User not authenticated");
     }
+
+    try {
+      return wrapped.apply(this, arguments); // call the original method within this new method
+    } catch (error) {
+      console.error(`Error in ${propertyKey}: ${error.message}`);
+      throw error;
+    }
+  };
 }
 
 class ContactRepository {
-    private contacts: Contact[] = [];
+  private contacts: Contact[] = [];
 
-    @authorize("ContactViewer")
-    getContactById(id: number): Contact | null {
-        if (!currentUser.isInRole("ContactViewer")) {
-            throw Error("User not authorized to execute this action");
-        }
-
-        const contact = this.contacts.find(x => x.id === id);
-        return contact;
+  @authorize
+  getContactById(id: number): Contact | null {
+    if (!currentUser.isInRole("ContactViewer")) {
+      throw Error("User not authorized to execute this action");
     }
 
-    @authorize("ContactEditor")
-    save(contact: Contact): void {
-        const existing = this.getContactById(contact.id);
+    const contact = this.contacts.find((x) => x.id === id);
+    return contact;
+  }
 
-        if (existing) {
-            Object.assign(existing, contact);
-        } else {
-            this.contacts.push(contact);
-        }
+  @authorize
+  save(contact: Contact): void {
+    const existing = this.getContactById(contact.id);
+
+    if (existing) {
+      Object.assign(existing, contact);
+    } else {
+      this.contacts.push(contact);
     }
+  }
 }
