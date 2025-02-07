24400d3d54a5b9617ef8ffd7a78d11bee692b13d
Abdul Arif <abdul.arif0705@gmail.com>
2025-02-06 20:10:44 -0500

    decorators

--- exercise_files/Ch05/05_01b/src/demo.ts
+++ exercise_files/Ch05/05_01b/src/demo.ts
@@ -1,47 +1,49 @@
 interface Contact {
-    id: number;
+  id: number;
 }
 
 const currentUser = {
-    id: 1234,
-    roles: ["ContactEditor"],
-    isInRole(role: string): boolean {
-        return this.roles.contains(role);
-    }
-}
+  id: 1234,
+  roles: ["ContactEditor"],
+  isInRole(role: string): boolean {
+    return this.roles.contains(role);
+  },
+};
 
+@log // instead of doing trace and debug it does it for us throughout the class functions
 class ContactRepository {
-    private contacts: Contact[] = [];
+  private contacts: Contact[] = [];
 
-    getContactById(id: number): Contact | null {
-        console.trace(`ContactRepository.getContactById: BEGIN`);
+  @authorize("ContactViewer")
+  getContactById(id: number): Contact | null {
+    // console.trace(`ContactRepository.getContactById: BEGIN`);
 
-        if (!currentUser.isInRole("ContactViewer")) {
-            throw Error("User not authorized to execute this action");
-        }
+    // if (!currentUser.isInRole("ContactViewer")) {
+    //   throw Error("User not authorized to execute this action");
+    // }
 
-        const contact = this.contacts.find(x => x.id === id);
+    const contact = this.contacts.find((x) => x.id === id);
 
-        console.debug(`ContactRepository.getContactById: END`);
+    // console.debug(`ContactRepository.getContactById: END`);
 
-        return contact;
+    return contact;
+  }
+  @authorize("ContactEditor")
+  save(contact: Contact): void {
+    // console.trace(`ContactRepository.save: BEGIN`);
+
+    // if (!currentUser.isInRole("ContactEditor")) {
+    //   throw Error("User not authorized to execute this action");
+    // }
+
+    const existing = this.getContactById(contact.id);
+
+    if (existing) {
+      Object.assign(existing, contact);
+    } else {
+      this.contacts.push(contact);
     }
 
-    save(contact: Contact): void {
-        console.trace(`ContactRepository.save: BEGIN`);
-
-        if (!currentUser.isInRole("ContactEditor")) {
-            throw Error("User not authorized to execute this action");
-        }
-
-        const existing = this.getContactById(contact.id);
-
-        if (existing) {
-            Object.assign(existing, contact);
-        } else {
-            this.contacts.push(contact);
-        }
-
-        console.debug(`ContactRepository.save: END`);
-    }
+    // console.debug(`ContactRepository.save: END`);
+  }
 }
