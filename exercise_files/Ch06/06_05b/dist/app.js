c9c09b21dc2e80d190dfd80a0d29a95cfa0701d9
Abdul Arif <abdul.arif0705@gmail.com>
2025-02-13 15:33:36 -0500

    add comments

--- exercise_files/Ch06/06_05b/dist/app.js
+++ exercise_files/Ch06/06_05b/dist/app.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", { value: true }); // This is a module for node.js to understand that this is a module. it does this because in tsconfig.json we have "module": "commonjs".
 const utils_1 = require("../dist/utils");
 const formattedDate = utils_1.formatDate(new Date());
 console.log(formattedDate);
